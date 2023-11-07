4ea0898533896a018b114a7b42c83cc13c40f0ae
IanGichane <gichaneian@gmail.com>
2023-11-07 14:14:09 +0300

    Navbar section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TopNav from "./Components/TopNav";
+import NavBar from "./Components/NavBar";
+import NavLogo from "./Components/NavLogo";
+import NavItem from "./Components/NavItem";
+import { BsCart4 } from "react-icons/bs";
+import { IoSearchOutline } from "react-icons/io5";
+import { VscMenu } from "react-icons/vsc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopNav />
+      <NavBar>
+        <>
+          <NavLogo/>
+          <NavItem icon={<IoSearchOutline />} />
+          <NavItem icon={<BsCart4 />} />
+          <NavItem icon={<VscMenu />} />
+        </>
+      </NavBar>
+    </>
   );
 }
 
