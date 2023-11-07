18153b88d7e5a0f20f8f4fbce6bf799de2a4fb36
John Johnson <jjmbeba@gmail.com>
2023-11-07 19:27:55 +0300

    feat: handleSubscribe function handled

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Newsletter = () => {
+  const [userEmail, setUserEmail] = useState("");
+
+  const handleSubscribe = () => {
+    //Email validation and subscribe logic
+    console.log(userEmail);
+  };
+
   return (
     <div className="bg-amber-950 py-28 px-16">
       <h2 className="text-[48px] leading-[120%] font-bold max-w-[768px] text-left text-white">
@@ -10,21 +17,22 @@ const Newsletter = () => {
         Subscribe to our blog and be the first to know about our exclusive
         coffee releases.
       </p>
-      <form action="">
-        <div className="flex items-center gap-1.5">
-          <input
-            type="text"
-            className="p-3 w-[394px] rounded-none"
-            placeholder="email@gmail.com"
-          />
-          <button
-            type="submit"
-            className="py-3 px-6 rounded-none bg-white text-amber-950 hover:bg-white hover:opacity-50 font-medium"
-          >
-            Subscribe
-          </button>
-        </div>
-      </form>
+      <div className="flex items-center gap-1.5">
+        <input
+          type="text"
+          className="p-3 w-[394px] rounded-none"
+          placeholder="email@gmail.com"
+          onChange={(e) => setUserEmail(e.target.value)}
+          value={userEmail}
+        />
+        <button
+          type="button"
+          onClick={handleSubscribe}
+          className="py-3 px-6 rounded-none bg-white text-amber-950 hover:bg-white hover:opacity-50 font-medium"
+        >
+          Subscribe
+        </button>
+      </div>
       <span className="mt-4 text-white text-[12px] leading-[150%]">
         By joining, you agree to our Terms and Conditions.
       </span>
