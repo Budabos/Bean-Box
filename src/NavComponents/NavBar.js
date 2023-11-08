0242fd39e6655ab3fc0dd9934f74015867f3c447
IanGichane <gichaneian@gmail.com>
2023-11-08 20:26:33 +0300

    Merge branch -ian

--- src/NavComponents/NavBar.js
+++ src/NavComponents/NavBar.js
@@ -1,6 +1,6 @@
 //component composition - break down application into small reusable functions
 const NavBar = (props) => {
-  return (
+  return (    
     <nav className="navbar">
       <ul className="navbar-lst">
         {props.children}
@@ -10,21 +10,3 @@ const NavBar = (props) => {
 };
 
 export default NavBar;
-    //   <>
-    //     <div>Logo</div>
-    //     <div>BrandNAme</div>
-    //     <div>
-    //       product
-    //       <ul>
-    //         <li>Coffee Beans</li>
-    //         <li>Coffee Beans</li>
-    //       </ul>
-    //     </div>
-    //     <div>Store</div>
-    //     <div>About us</div>
-    //     <div>NavBar</div>
-    //     <div>Stories</div>
-    //     <div>Search</div>
-    //     <div>Cart</div>
-    //     <div>HumBurger Menu</div>
-    //   </>
