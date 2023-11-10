93863c26f76beb9b8e523a159ecc6179401113a4
caroline opiyo <opiyocaroline20@gmail.com>
2023-11-10 21:32:47 +0300

    Adding comments in index.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CartProvider } from "./context/cartContext";
-
+// this code initializes a React application with advanced development features 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
