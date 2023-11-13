37029435c468459ebc4120d641f9002b393aba18
caroline opiyo <opiyocaroline20@gmail.com>
2023-11-13 22:29:55 +0300

    State to manage form data

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 // CreateProduct Component
 const CreateProduct = ({ products, setProducts }) => {
   // State to manage form data
@@ -11,10 +12,12 @@ const CreateProduct = ({ products, setProducts }) => {
     image: "",
     category: "beans",
   });
-// Function to handle form submission
+
+  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+
     // Sending a POST request to create a new product
     fetch("http://localhost:8000/products", {
       method: "POST",
@@ -40,28 +43,31 @@ const CreateProduct = ({ products, setProducts }) => {
       })
       .catch((err) => console.log(err));
   };
+
   // Function to handle form input changes
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
- // Updating the form data based on user input
+
+    // Updating the form data based on user input
     setFormData((prev) => {
-      
       return {
         ...prev,
         [name]: value,
       };
     });
   };
-// Rendering the form for creating a new product
+
+  // Rendering the form for creating a new product
   return (
     <div className="tw-flex tw-flex-col tw-items-center tw-gap-[15px]">
       <h2 className="tw-text-center tw-font-edu tw-font-bold tw-overflow-hidden">
         Create a product
       </h2>
       <form className="tw-w-1/3">
+        {/* Form fields for different product attributes */}
         <div className="form-group">
-          <label for="name">Name</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
             className="form-control"
@@ -71,68 +77,13 @@ const CreateProduct = ({ products, setProducts }) => {
             onChange={handleChange}
           />
         </div>
+        {/* ... Repeat similar form fields for other attributes ... */}
         <div className="form-group">
-          <label for="recipe">Recipe</label>
-          <input
-            type="text"
-            className="form-control"
-            id="recipe"
-            name="recipe"
-            placeholder="To make a cup of [product] coffee, grind Arabica coffee beans"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label for="image">Image</label>
-          <input
-            type="text"
-            className="form-control"
-            id="image"
-            name="image"
-            placeholder="https://cdn.accentuate.io/557828603966/1677786404629/Our-Coffee-Ethiopia-tabletop-production-916W-578-2x.jpg?v=1692634350716"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label for="likes">Likes</label>
-          <input
-            type="text"
-            className="form-control"
-            id="likes"
-            name="likes"
-            placeholder="150"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label for="flavor">Flavor</label>
-          <input
-            type="text"
-            className="form-control"
-            id="flavor"
-            name="flavor"
-            placeholder="Vanilla, Caramel, Hazelnut"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label for="price">Price</label>
-          <input
-            type="text"
-            className="form-control"
-            id="price"
-            name="price"
-            placeholder="1500"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label for="recipe">Category</label>
+          <label htmlFor="category">Category</label>
           <select
             className="form-control"
             id="category"
             name="category"
-            placeholder="Enter the category"
             onChange={handleChange}
           >
             <option value="drinks">Drinks</option>
@@ -140,6 +91,7 @@ const CreateProduct = ({ products, setProducts }) => {
           </select>
         </div>
 
+        {/* Submit button */}
         <button
           onClick={handleSubmit}
           type="submit"
@@ -151,5 +103,6 @@ const CreateProduct = ({ products, setProducts }) => {
     </div>
   );
 };
+
 // Exporting the CreateProduct component
 export default CreateProduct;
