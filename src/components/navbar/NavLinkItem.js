9f67db125c9d3152d108896429208c07d4819364
John Johnson <jjmbeba@gmail.com>
2023-11-13 21:29:02 +0300

    enh: styles refactor

--- src/components/navbar/NavLinkItem.js
+++ src/components/navbar/NavLinkItem.js
@@ -2,13 +2,12 @@ import { Link } from "react-router-dom";
 
 const NavLinkItem = ({ text, links, redirect }) => {
   return (
-    <li className="tw-overflow-hidden tw-relative after:tw-absolute after:tw-content-[''] after:tw-bottom-[0px] after:tw-w-full after:tw-h-[2px] after:tw-bg-black after:tw-scale-100 hover:after:tw-scale-100 after:tw-transition-all">
+    <li className="tw-overflow-hidden ">
       <Link to={redirect} className="tw-no-underline">
-        <span className="dropbtn tw-capitalize tw-bg-white tw-text-black tw-font-semibold">
+        <span className="dropbtn tw-capitalize tw-bg-white tw-text-black tw-font-semibold tw-relative after:tw-absolute after:tw-content-[''] after:tw-bottom-[-5px] after:tw-left-0 after:tw-w-full after:tw-h-[2px] after:tw-bg-black after:tw-scale-0 hover:after:tw-scale-100 after:tw-transition-all">
           {text}
         </span>
-      </Link>
-      <div class="dropdown-content">{links}</div>
+      </Link>{" "}
     </li>
   );
 };
